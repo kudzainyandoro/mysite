075e537d20187be698a4050c27ee0f02589d5fea
kudzainyandoro <kudzain@gmail.com>
2020-10-29 06:47:55 -0700

    Correct blog/url.py to blog/urls.py

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -27,6 +28,14 @@ class Post(models.Model):
                               default='draft')
     objects = models.Manager() #Default manager
     published = PublishedManager() #Custom manager
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
 
     class Meta:
         ordering = ('-publish',)
