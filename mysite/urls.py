d6113e0676d6cc75574318f864de6d7dd26d6507
kudzainyandoro <kudzain@gmail.com>
2020-10-29 06:15:21 -0700

    Quick update

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
-from django.contrib import admin, inlcude
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
